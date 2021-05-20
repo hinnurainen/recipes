1bb956df4baae31846b029c51a1e69238bb0be5c
hinnurainen <heidi.maattanen@gmail.com>
2021-05-20 10:35:16 +0300

    change favicon

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,15 +4,15 @@ const About = () => {
     return (
         <div>
             <h1>This is a school project on React single page application where we are using:</h1>
-            <p>- function components</p>
-            <p>- class components</p>
-            <p>- event handlers</p>
-            <p>- conditional rendering</p>
-            <p>- lists and keys</p>
-            <p>- forms and controlled components</p>
-            <p>- routing</p>
-            <p>- get and post to database</p>
-            <p>- styling with component library</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> function components</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> class components</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> event handlers</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> conditional rendering</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> lists and keys</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> forms and controlled components</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> routing</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> get and post to database</p>
+            <p><img src="favicon-16x16.png" alt="banana" /> styling with component library</p>
         </div>
     );
 };
